bd027e2911ccb3ca527aed1ad645a0262ed69034
G. Tzempelikos <gtzempe@gmail.com>
2025-12-16 01:53:09 +0200

    feat: add geolocation functionality and update dashboard with location data

--- src/pages/Weather-dashboard.tsx
+++ src/pages/Weather-dashboard.tsx
@@ -1,7 +1,76 @@
-
+import { AlertCircle, AlertTriangle, MapPin, RefreshCw } from "lucide-react";
+import { Button } from "../components/ui/button";
+import { useGeolocation } from "../hooks/use-geolocation";
+import WeatherSkeleton from "../components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert";
 
 const WeatherDasboard = () => {
-    return <div>WeatherDashboard</div>
-}
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeolocation();
 
-export default WeatherDasboard
+  const handleRefresh = () => {
+    getLocation();
+    if (coordinates) {
+    }
+  };
+
+  if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!coordinates) {
+    return (
+      <Alert variant="destructive">
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please enable location access to see your local weather.</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  return (
+    <div className="space-y-4">
+      {/* Fovorite Cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          // onClick={handleRefresh}
+          // disabled
+        >
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
+
+      {/* Current and Hourly weather */}
+    </div>
+  );
+};
+
+export default WeatherDasboard;
