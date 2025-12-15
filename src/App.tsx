1f4b18b0eff14f0654f035a4948163cdb674fb47
G. Tzempelikos <gtzempe@gmail.com>
2025-12-15 18:11:56 +0200

    feat: add layout component with routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout'
 
 function App() {
   return (
-  <BrowserRouter>
-    
-  </BrowserRouter>
+    <BrowserRouter>
+      <Layout>Hello</Layout>
+    </BrowserRouter>
   )
 }
 
