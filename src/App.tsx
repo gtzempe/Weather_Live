caab7e28029f4c3cee2706374b3a68f3302c9190
G. Tzempelikos <gtzempe@gmail.com>
2025-12-15 18:18:00 +0200

    feat: dark mode added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
+import { ThemeProvider } from './context/theme-provider'
 
 function App() {
   return (
     <BrowserRouter>
+    <ThemeProvider defaultTheme='dark'>
       <Layout>Hello</Layout>
+    </ThemeProvider>
     </BrowserRouter>
   )
 }
