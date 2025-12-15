cd8d17911d8399b541c84bb8e59f424cd1a81850
G. Tzempelikos <gtzempe@gmail.com>
2025-12-15 19:29:33 +0200

    feat: header and complete dark mode

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,20 @@
-import type { PropsWithChildren } from "react"
+import type { PropsWithChildren } from "react";
+import Header from "./header";
 
-
-const Layout = ({children}: PropsWithChildren) => {
-    return (
-        <div className="bg-linear-to-br from-background to-muted">
-            header
-            <main className="min-h-screen container mx-auto px-4 py-8">
-                {children}
-            </main>
-            <footer className="border-t backdrop-blur py-12 supports-backdrop-filter:bg-background/60">
-                <div className="container mx-auto px-4 text-center text-gray-400">
-                <p>Lets make the next move...</p>
-                </div>
-            </footer>
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="bg-linear-to-br from-background to-muted">
+      <Header />
+      <main className="min-h-screen container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <footer className="border-t backdrop-blur py-12 supports-backdrop-filter:bg-background/60">
+        <div className="container mx-auto px-4 text-center text-gray-400">
+          <p>Lets make the next move...</p>
         </div>
-    )
-}
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
