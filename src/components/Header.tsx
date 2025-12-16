5a91dc057a869d94df6c43a87beff879790bf786
G. Tzempelikos <gtzempe@gmail.com>
2025-12-16 19:22:57 +0200

    feat: created ThemeToggle component and header refactored for cleaner code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,27 @@
 import { Link } from "react-router-dom";
+import { CitySearch } from "./City-search";
+import { ThemeToggle } from "./Theme-toggle";
 import { useTheme } from "../context/theme-provider";
-import { Moon, Sun } from "lucide-react";
 
-const Header = () => {
-  const { theme, setTheme } = useTheme();
-  const isDark = theme === "dark";
+export function Header() {
+  const { theme } = useTheme();
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-backdrop-filter:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60 py-2">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to={"/"}>
           <img
-            src={isDark ? "/logoDark.png" : "/logoWhite.png"}
-            alt="Weather Live"
+            src={theme === "dark" ? "/logoDark.png" : "/logoWhite.png"}
+            alt="Live Weather"
             className="h-14"
           />
         </Link>
 
-        <div>
-          {/* search */}
-          {/* theme toggle */}
-          <div
-            onClick={() => setTheme(isDark ? "light" : "dark")}
-            className={`flex items-center cursor-pointer transition-transform duration-500 ${
-              isDark ? "rotate-180" : "rotate-0"
-            }`}
-          >
-            {isDark ? (
-              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
-            ) : (
-              <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
-            )}
-          </div>
+        <div className="flex gap-4">
+          <CitySearch />
+          <ThemeToggle />
         </div>
       </div>
     </header>
   );
-};
-
-export default Header;
+}
