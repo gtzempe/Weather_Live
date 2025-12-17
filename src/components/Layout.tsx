0edf4ba498401f5bc2c8682a0cacbf64637c6973
G. Tzempelikos <gtzempe@gmail.com>
2025-12-17 13:10:53 +0200

    refactor: changed footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: PropsWithChildren) => {
       </main>
       <footer className="border-t backdrop-blur py-12 supports-backdrop-filter:bg-background/60">
         <div className="container mx-auto px-4 text-center text-gray-400">
-          <p>Lets make the next move...</p>
+          <p>Weather changes. Stay informed.</p>
         </div>
       </footer>
     </div>
